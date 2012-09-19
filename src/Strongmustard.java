d30913db683c6dd5cd4c67ed093263e784ca6de5
anthony.hanson <anthony.hanson@MH-208-08.msbstudents.priv>
2012-09-18 19:30:44 -0500

    Adding Tony's

---
+++ src/Strongmustard.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author anthony.hanson
+ */
+public class Strongmustard {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PayPeriod pp = new PayPeriod();
+        
+        pp.wage = "16.50";
+        pp.fileName = "c:/extra/week1.smp";
+        pp.startDate="9/16/2012";
+        pp.payPeriodType = "BIWEEKLY";
+        
+        // write to a file 
+    }
+}
