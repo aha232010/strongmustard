9d7685bdd84e7a260131a2774a4baa0445185179
anthony.hanson <anthony.hanson@MH-208-08.msbstudents.priv>
2012-09-18 19:30:33 -0500

    Adding Jims

---
+++ src/PayPeriod.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author James.Einarson
+ */
+public class PayPeriod {
+    String startDate;
+    String fileName;
+    String payPeriodType;
+    String wage;
+    String payPeriodEnd;
+    Details[] details;
+}
