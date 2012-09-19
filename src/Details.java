724dc004794654ad594fc8be003d560fbeb34317
anthony.hanson <anthony.hanson@MH-208-08.msbstudents.priv>
2012-09-18 19:30:26 -0500

    Adding Jims

---
+++ src/Details.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author James.Einarson
+ */
+public class Details {
+    String start;
+    String end;
+    String lunchStart;
+    String lunchEnd;
+    String notes;
+}
